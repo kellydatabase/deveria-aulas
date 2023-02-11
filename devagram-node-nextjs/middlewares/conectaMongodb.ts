9161bdfd5b0e389c0910bfd63a0b3a1d059a9737
Kelly Rodrigues <122828003+kellydatabase@users.noreply.github.com>
2023-02-11 15:03:20 -0300

    configuração do banco de dados

---
+++ devagram-node-nextjs/middlewares/conectaMongodb.ts
@@ -0,0 +1,30 @@
+import type { NextApiRequest, NextApiResponse, NextApiHandler } from 'next';
+import mongoose from 'mongoose';
+import { error } from 'console';
+import {RespostaPadraoMsg} from '../types/RespostaPadraoMSG';
+
+export const conectarMongoDB = (handler : NextApiHandler) =>
+    async (req: NextApiRequest, res : NextApiResponse<RespostaPadraoMsg>) => {
+        
+        //verificar se o banco se já esta conectado, se tiver seguir para
+        //o endpoint ou proximo middleware
+        if(mongoose.connections[0].readyState){
+            return handler(req, res);
+        }
+        //ja que não esta conectado vamos conectar
+        //obter a variavel de ambiente preenchida do env
+        const {DB_CONEXAO_STRING} = process.env;
+
+        //se a env estiver vazia aborta o uso do sistema e avisa o programador
+        if(!DB_CONEXAO_STRING){
+            return res.status(500).json({erro : 'Env de configuração do banco, não informado'})
+    }
+    mongoose.connection.on('connected', () => console.log('Banco de dados conectado'));
+    mongoose.connection.on('error', error => console.log('Ocorreu erro ao conectar no banco'));
+    await mongoose.connect(DB_CONEXAO_STRING);
+    
+    //agora posso seguir para o endpoint, pois estou conectado
+    //no banco
+
+    return handler(req, res);
+}
--- devagram-node-nextjs/pages/api/login.ts
+++ devagram-node-nextjs/pages/api/login.ts
@@ -1,19 +1,21 @@
-//exemplo de login API
-
 import type {NextApiRequest, NextApiResponse} from "next";
+import {conectarMongoDB} from '../../middlewares/conectaMongodb';
+import type {RespostaPadraoMsg} from '../../types/RespostaPadraoMSG';
 
-export default(
+const endpointLogin =(
     req : NextApiRequest,
-    res : NextApiResponse
+    res : NextApiResponse <RespostaPadraoMsg>
 ) => {
     if(req.method === 'POST'){
         const {login, senha} = req.body;
         
         if(login === 'admin@admin.com' &&
             senha === 'Admin@123'){
-            res.status(200).json({msg : 'Usuário autenticado com sucesso'});
+            return res.status(200).json({msg : 'Usuário autenticado com sucesso'});
         }
         return res.status(405).json({erro : 'Usuário ou senha não encontrato'});
     }
     return res.status(405).json({erro : 'Método informado não é válido'});
   }
+
+  export default conectarMongoDB(endpointLogin);
