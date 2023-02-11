90bcb1b423d309e871d67d373c1e4e4a7548eb1f
Kelly Rodrigues <122828003+kellydatabase@users.noreply.github.com>
2023-02-11 18:26:21 -0300

    Inicio integração JWT

--- devagram-node-nextjs/pages/api/login.ts
+++ devagram-node-nextjs/pages/api/login.ts
@@ -1,21 +1,41 @@
 import type {NextApiRequest, NextApiResponse} from "next";
 import {conectarMongoDB} from '../../middlewares/conectaMongodb';
 import type {RespostaPadraoMsg} from '../../types/RespostaPadraoMSG';
+import type {LoginResposta} from '../../types/LoginResposta';
+import md5 from "md5";
+import { UsuarioModel } from "@/models/UsuarioModels";
+import jwt from 'jsonwebtoken';
 
-const endpointLogin =(
+const endpointLogin = async (
     req : NextApiRequest,
-    res : NextApiResponse <RespostaPadraoMsg>
+    res : NextApiResponse <RespostaPadraoMsg | LoginResposta>
 ) => {
+    const {MINHA_CHAVE_JWT} = process.env;
+    if (!MINHA_CHAVE_JWT){
+        res.status(500).json({erro: 'ENV Jwt não informada'});
+    }
+
+
     if(req.method === 'POST'){
         const {login, senha} = req.body;
         
-        if(login === 'admin@admin.com' &&
-            senha === 'Admin@123'){
+        const usuariosEncontrados = await UsuarioModel.find({email: login, senha: md5(senha)});
+        if(usuariosEncontrados && usuariosEncontrados.length >0){
+            const usuarioEncontrado = usuariosEncontrados [0];
+            
+            const token =  jwt.sign({_id : usuarioEncontrado._id}, MINHA_CHAVE_JWT);
+            
+           { return res.status(200).json({
+            nome : usuarioEncontrado.nome, 
+            email : usuarioEncontrado.email, 
+            token});
+        }
+
             return res.status(200).json({msg : 'Usuário autenticado com sucesso'});
         }
         return res.status(405).json({erro : 'Usuário ou senha não encontrato'});
     }
     return res.status(405).json({erro : 'Método informado não é válido'});
-  }
-
+  
+}
   export default conectarMongoDB(endpointLogin);
---
+++ devagram-node-nextjs/types/LoginResposta.ts
@@ -0,0 +1,6 @@
+export type LoginResposta = {
+    nome : string,
+    email : string,
+    token : string
+}
+
